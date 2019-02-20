9d96b88e98bf9e248ac0ef4282bf9e3a58d3e43d
iwfan <i.wangfancn@gmail.com>
2019-02-20 11:45:43 +0800

    Updates

---
+++ precache-manifest.26a7e586d33b04f25284619bf602f8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "2fd52cfe5f6650ab5af2",
+    "url": "./static/js/main.2fd52cfe.chunk.js"
+  },
+  {
+    "revision": "76331fe0f92fcb607f53",
+    "url": "./static/js/1.76331fe0.chunk.js"
+  },
+  {
+    "revision": "2fd52cfe5f6650ab5af2",
+    "url": "./static/css/main.1af1e944.chunk.css"
+  },
+  {
+    "revision": "76331fe0f92fcb607f53",
+    "url": "./static/css/1.f96a53be.chunk.css"
+  },
+  {
+    "revision": "2fe1fa4ad60646cbfa66cf3724be4771",
+    "url": "./index.html"
+  }
+];
