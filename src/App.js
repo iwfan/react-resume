89a5848c5a468181c97f28029b856cdbe77f1a02
iwfan <i.wangfancn@gmail.com>
2019-02-20 11:40:07 +0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,82 +1,115 @@
-import React, { Component } from 'react'
-import Editor from './components/Editor'
-import Resume from './components/Resume'
-import './App.css'
+import React, { Component } from 'react';
+import Editor from './components/Editor';
+import Resume from './components/Resume';
+import './App.css';
 
 /** 加载文本文件 */
-const loadRawFile = require.context('!raw-loader!./', true, /\.(md|css|txt)$/)
-const introCss = loadRawFile('./styles/intro.css')
-const setEditorCss = loadRawFile('./styles/setEditor.css')
-const setResumeEditorCss = loadRawFile('./styles/setResumeEditor.css')
-const endCss = loadRawFile('./styles/end.css')
-const md = loadRawFile('./resume.md')
+const loadRawFile = require.context('!raw-loader!./', true, /\.(md|css|txt)$/);
+const introCss = loadRawFile('./styles/intro.css');
+const setEditorCss = loadRawFile('./styles/setEditor.css');
+const setResumeEditorCss = loadRawFile('./styles/setResumeEditor.css');
+const endCss = loadRawFile('./styles/end.css');
+const md = loadRawFile('./resume.md');
 
-const timeout = async ms => new Promise(resolve => setTimeout(resolve, ms))
+const timeout = async ms => new Promise(resolve => setTimeout(resolve, ms));
 
 class App extends Component {
   speed = {
     slow: 90,
     mid: 40,
-    fast: 10
-  }
+    fast: 20
+  };
   state = {
     styleCode: '',
     speed: this.speed.fast,
-    pause: false,
+    skip: false,
     markdown: '',
     enableLineNum: false
-  }
+  };
+  codeRef = React.createRef();
+  mdRef = React.createRef();
   componentDidMount() {
-    this.makeResume()
+    this.makeResume();
   }
   async makeResume() {
-    await this.progressiveShowWord(introCss, this.state.speed, 'styleCode')
-    await timeout(500)
-    await this.progressiveShowWord(setEditorCss, this.state.speed, 'styleCode')
-    this.setState({ enableLineNum: true })
-    await timeout(500)
-    await this.progressiveShowWord(
-      setResumeEditorCss,
-      this.state.speed,
-      'styleCode'
-    )
-    await this.progressiveShowWord(md, this.state.speed, 'markdown')
-    await timeout(500)
-    await this.progressiveShowWord(endCss, this.state.speed, 'styleCode')
+    try {
+      await this.progressiveShowWord(introCss, this.state.speed, 'styleCode');
+      await timeout(500);
+      await this.progressiveShowWord(
+        setEditorCss,
+        this.state.speed,
+        'styleCode'
+      );
+      this.setState({ enableLineNum: true });
+      await timeout(500);
+      await this.progressiveShowWord(
+        setResumeEditorCss,
+        this.state.speed,
+        'styleCode'
+      );
+      await this.progressiveShowWord(md, this.state.speed, 'markdown');
+      await timeout(500);
+      await this.progressiveShowWord(endCss, this.state.speed, 'styleCode');
+    } catch (exception) {
+      this.setState(
+        {
+          styleCode: introCss + setEditorCss + setResumeEditorCss + endCss,
+          markdown: md,
+          enableLineNum: true
+        },
+        () => {
+          this.mdRef.current.scrollTop = 0;
+        }
+      );
+    }
   }
+  skipAnimation = () => {
+    if (this.state.skip !== true) {
+      this.setState({ skip: true });
+    }
+  };
   async progressiveShowWord(text, speed, target) {
-    return new Promise(resolve => {
-      let timer = null
-      let wordCount = 0
+    return new Promise((resolve, reject) => {
+      let timer = null;
+      let wordCount = 0;
       timer = setInterval(() => {
-        if (this.state.pause) {
-          return
+        if (this.state.skip) {
+          clearInterval(timer);
+          reject('skip');
+          return;
         }
         this.setState(
           prev => {
-            return { [target]: prev[target] + text.substr(wordCount, 1) }
+            return { [target]: prev[target] + text.substr(wordCount, 1) };
           },
           () => {
             if (wordCount >= text.length) {
-              clearInterval(timer)
-              resolve()
+              clearInterval(timer);
+              resolve();
             }
-            wordCount += 1
+            wordCount += 1;
           }
-        )
-      }, speed)
-    })
+        );
+      }, speed);
+    });
   }
   render() {
     return (
       <React.StrictMode>
-        <div className='App'>
-          <Editor code={this.state.styleCode} enableLineNum={this.state.enableLineNum} forwardRef={this.codeRef} />
+        <div className="App">
+          <Editor
+            code={this.state.styleCode}
+            enableLineNum={this.state.enableLineNum}
+            forwardRef={this.codeRef}
+          />
           <Resume content={this.state.markdown} forwardRef={this.mdRef} />
+          <div className="control-bar">
+            <button onClick={this.skipAnimation}>跳过动画</button>
+          </div>
         </div>
       </React.StrictMode>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/AutoAdjustScrollHOC.jsx
+++ src/components/AutoAdjustScrollHOC.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 /**
  * 自动调整滚动条的高度
  */
 export default WrappedComponent => {
   return class AutoAdjustScroll extends React.PureComponent {
-    ref = React.createRef()
+    // ref = React.createRef()
+    constructor(props) {
+      super(props);
+      this.ref = props.forwardRef;
+    }
     componentDidMount() {
-      this.adjustScrollPosition()
+      this.adjustScrollPosition();
     }
     componentDidUpdate() {
-      this.adjustScrollPosition()
+      this.adjustScrollPosition();
     }
     adjustScrollPosition() {
-      const { scrollHeight, scrollTop, clientHeight } = this.ref.current
+      const { scrollHeight, scrollTop, clientHeight } = this.ref.current;
       if (scrollHeight > scrollTop + clientHeight) {
-        this.ref.current.scrollTop = scrollHeight - clientHeight
+        this.ref.current.scrollTop = scrollHeight - clientHeight;
       }
     }
     render() {
-      const { ref, ...resetProps } = this.props
-      return <WrappedComponent {...resetProps} forwardRef={this.ref} />
+      const { ref, ...resetProps } = this.props;
+      return <WrappedComponent {...resetProps} forwardRef={this.ref} />;
     }
-  }
-}
+  };
+};
